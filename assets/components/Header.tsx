1d7851a9160f0b38ca22330e4bdf1df00d1fd7ad
Lastor-Chen <lastoraya@gmail.com>
2022-01-07 19:00:03 +0800

    [加入] Header 組件，調整版 svg files

--- assets/components/Header.tsx
+++ assets/components/Header.tsx
@@ -1,8 +1,47 @@
 import type { NextPage } from 'next'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const Header: NextPage = function () {
+  const btnNames = ['light', 'bell', 'member']
+
   return (
-    <header>Header</header>
+    <header className="d-flex justify-content-between align-items-center px-3">
+      <Link href="/">
+        <a className="next-img-fix">
+          <Image src="/images/kado-logo.svg" width="64" height="64" alt="logo" />
+        </a>
+      </Link>
+
+      <div className="d-flex">
+        {btnNames.map((btn, idx) => (
+          <button className="btn next-img-fix ms-4 p-0" key={`b${idx}`} disabled>
+            <div className={`icon ${btn}`}></div>
+          </button>
+        ))}
+      </div>
+
+      <style jsx>{`
+        .icon {
+          width: 1.5rem;
+          height: 1.5rem;
+          background-color: gray;
+          mask: no-repeat center;
+        }
+
+        .bell {
+          mask: url(/images/bell.svg);
+        }
+
+        .member {
+          mask: url(/images/member.svg);
+        }
+
+        .light {
+          mask: url(/images/switch-light.svg);
+        }
+      `}</style>
+    </header>
   )
 }
 
