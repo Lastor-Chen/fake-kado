dd17d83c1ea50786609fc14fd92f502a731a5b83
Lastor-Chen <lastoraya@gmail.com>
2022-01-07 11:47:05 +0800

    [加入] 引入 Bootstrap 5，調整 Head meta，移除預設內容

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
-import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="A Next.js learning project" />
+        <meta name="og:title" content="Fake-Kado | learning project" />
+        <meta name="og:description" content="A Next.js learning project" />
+        <meta name="og:image" content="https://nextjs.org/static/twitter-cards/learn-twitter.png" />
+        <link rel="icon" href="/favicon.ico" />
+        <title>Fake-Kado</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
-export default MyApp
+export default App
