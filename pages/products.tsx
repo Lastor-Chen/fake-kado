6b76a73f50a8ee333d27692a359b7afb86b62fe9
Lastor-Chen <lastoraya@gmail.com>
2022-01-13 15:31:57 +0800

    [途中] Products 頁，改用 useSWR，引入 react-if

--- pages/products.tsx
+++ pages/products.tsx
@@ -2,57 +2,74 @@ import Layout from '@components/Layout'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
-import { useEffect, useState } from 'react'
 import axios from 'axios'
 import ProductCard from '@components/ProductCard'
 import type { Book } from '@seeds/books'
+import useSWR from 'swr'
+import { waitTime, handleAxiosError } from '@utils/tool'
+import { When } from 'react-if'
 
 interface ProductsResponse {
   status: string,
   result: Book[]
 }
 
-const Products: NextPage = () => {
-  const [books, setBooks] = useState<Book[]>([])
-  console.log('books', books)
+async function fetchBooks(url: string) {
+  // 模擬 loading 延遲
+  if (process.env.NODE_ENV === 'development') {
+    await waitTime(1500)
+  }
 
-  useEffect(() => {
-    if (!books.length) {
-      // 假 Loading 延遲
-      setTimeout(() => {
-        axios.get<ProductsResponse>('/api/products').then(({ data }) => {
-          const books = data.result
-          setBooks(books)
-        })
-      }, 1500)
-    }
-  })
+  const { data } = await axios.get<ProductsResponse>(url)
+  if (data.status !== 'ok') throw new Error('Server Error')
+  return data
+}
+
+const Products: NextPage = function () {
+  const { data, error } = useSWR('/api/products', fetchBooks)
+  if (error) { handleAxiosError(error) }
+
+  const books = data?.result
 
   return (
     <Layout>
       <Head>
-        <title>Fake-Kado | Products</title>
+        <title>Fake-Kado | Books</title>
       </Head>
 
-      {!books.length ? (
-        <div>Loading...</div>
-      ) : (
-        <article>
-          <div className="row">
-            {books.map((book) => (
-              <div className="col-6 d-flex" key={book.id}>
-                <div className="w-50">
-                  <img src={book.image} className="w-100" alt="" />
-                </div>
-                <div className="w-50 px-3">
-                  <h4>{book.name}</h4>
-                  <div>{book.author}</div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </article>
-      )}
+      <When condition={error}>
+        <div className="text-center">Failed to fetch data.</div>
+      </When>
+
+      <When condition={!books && !error}>
+        <div className="text-center">Loading...</div>
+      </When>
+
+      <When condition={books?.length}>
+        {() => (
+          <section>
+            <div className="row">
+              {books?.map((book) => (
+                <article className="col-6 d-flex" key={book.id}>
+                  <div className="w-50">
+                    <Link href={`/product/${book.id}`}>
+                      <a className="d-inline-block">
+                        <img src={book.image} className="w-100" alt="" />
+                      </a>
+                    </Link>
+                  </div>
+                  <div className="w-50 px-3">
+                    <Link href={`/product/${book.id}`}>
+                      <a className="h4 text-reset text-decoration-none">{book.name}</a>
+                    </Link>
+                    <div>{book.author}</div>
+                  </div>
+                </article>
+              ))}
+            </div>
+          </section>
+        )}
+      </When>
 
       <style jsx>{`
         .test {
