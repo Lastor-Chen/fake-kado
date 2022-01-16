43e9b4f61307c96736f0898247f075fca9f8543f
Lastor-Chen <lastoraya@gmail.com>
2022-01-17 01:04:30 +0800

    [途中] Products 頁，加入下拉分頁 (前端)

--- pages/products.tsx
+++ pages/products.tsx
@@ -5,13 +5,17 @@ import axios from 'axios'
 import ProductCard from '@components/ProductCard'
 import type { ProductsResponse } from './api/products'
 import useSWR from 'swr'
-import { waitTime, handleAxiosError } from '@utils/tool'
+import { handleAxiosError, waitTime } from '@utils/tool'
 import { When } from 'react-if'
 import CategoryBar from '@components/CategoryBar'
 import SearchBar from '@components/SearchBar'
 import Spinner from '@assets/components/Spinner'
+import { useEffect, useState } from 'react'
+import type { Book } from '@assets/seeds/books'
 
 async function fetchBooks(url: string) {
+  await waitTime(1500)
+
   const { data } = await axios.get<ProductsResponse>(url)
   if (data.status !== 'ok') throw new Error('Server Error')
   return data
@@ -19,12 +23,15 @@ async function fetchBooks(url: string) {
 
 // SSG without data + CSR Page
 const Products: NextPage = function () {
+  const [pageIdx, setPageIdx] = useState(1)
   const { data, error } = useSWR('/api/products?order=DESC', fetchBooks)
   if (error) {
     handleAxiosError(error)
   }
 
-  const books = data?.results
+  const limit = 10
+  const offset = pageIdx * limit
+  const books = data?.results.slice(0, offset)
 
   return (
     <Layout hasNav>
@@ -56,6 +63,11 @@ const Products: NextPage = function () {
                   />
                 ))}
               </div>
+              <div className="mt-4 text-center">
+                <button className="more-btn btn-primary small fw-bold" onClick={() => setPageIdx(pageIdx + 1)}>
+                  看更多
+                </button>
+              </div>
             </section>
           )}
         </When>
@@ -64,6 +76,22 @@ const Products: NextPage = function () {
       <style jsx>{`
         .container.override {
           max-width: 1024px;
+        }
+
+        .more-btn {
+          display: inline-block;
+          width: 75%;
+          max-width: 256px;
+          border-radius: 50rem;
+          border: none;
+          background-color: transparent;
+        }
+
+        .btn-primary {
+          color: white;
+          background-color: var(--theme-ui-colors-primary);
+          border: 1px solid white;
+          padding: 0.75rem 0;
         }
       `}</style>
     </Layout>
