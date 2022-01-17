1dab98e81d00d9895a18ffdddc3fd56df3c28871
Lastor-Chen <lastoraya@gmail.com>
2022-01-17 11:13:50 +0800

    [途中] 後端 GET Products API，加入分頁機制

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -7,6 +7,10 @@ import type { APIResponse } from '@utils/api/types'
 export type ProductsQueryString = {
   q?: string
   order?: 'ASC' | 'DESC'
+  /** 字串 number, 一頁幾筆 */
+  limit?: string
+  /** 字串 number, 第幾頁 */
+  page?: string
 }
 interface ProductsRequest extends NextApiRequest {
   query: ProductsQueryString
@@ -15,6 +19,7 @@ interface ProductsRequest extends NextApiRequest {
 /** 定義此 API 回傳值 */
 export interface ProductsResponse extends APIResponse {
   results: Book[]
+  totalPage?: number
 }
 
 export default function controller(req: ProductsRequest, res: NextApiResponse<ProductsResponse>) {
@@ -23,15 +28,10 @@ export default function controller(req: ProductsRequest, res: NextApiResponse<Pr
   }
 
   // Search 請求
-  let order = req.query.order
-  if (order !== 'ASC' && order !== 'DESC') {
-    order = 'DESC'
-  }
-
-  let results = books
+  let searchedBooks: Book[] = books
   if (req.query.q) {
     const keyword = req.query.q || ''
-    results = books.filter(
+    searchedBooks = books.filter(
       (book) =>
         book.name.includes(keyword) ||
         book.categories.some((cateName) => cateName.includes(keyword)) ||
@@ -39,8 +39,27 @@ export default function controller(req: ProductsRequest, res: NextApiResponse<Pr
     )
   }
 
-  // Sort by DESC
-  results.sort((a, b) => b.id - a.id)
+  // Sort
+  let order = req.query.order!
+  if (order !== 'ASC' && order !== 'DESC') {
+    order = 'DESC'
+  }
+  searchedBooks.sort((a, b) => {
+    if (order === 'DESC') return b.id - a.id
+    else return a.id - b.id
+  })
 
-  res.status(200).json({ status: 'ok', results: results })
+  // 切分頁
+  let results = searchedBooks
+  let totalPage: number | undefined = undefined
+  if (req.query.limit && req.query.page) {
+    const limit = Number(req.query.limit)
+    const page = Number(req.query.page)
+    totalPage = Math.ceil(searchedBooks.length / limit)
+
+    const offset = limit * (page - 1)
+    results = searchedBooks.slice(offset, limit * page)
+  }
+
+  res.status(200).json({ status: 'ok', totalPage, results })
 }
